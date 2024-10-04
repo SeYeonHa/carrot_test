dc428ad16a98fbb9fe5fd3823c97ca1e1f952a5d
haseyeon <hase970853@gmail.com>
2024-10-04 17:47:48 +0900

    fix : Scaffold + appBar

--- lib/screens/near_me/near_me_screen.dart
+++ lib/screens/near_me/near_me_screen.dart
@@ -3,24 +3,67 @@ import 'package:flutter/material.dart';
 class NearMeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppBar(), // 앱바
-        SizedBox(), // 앱바 밑에 줄
-        TextFormField(), // 검색창
-        ListView(), // 태그
-        SizedBox(), // 회색 줄
-        Wrap(), // 아이콘 나열
-        Container(
-          // 이웃들의 추천가게
-          child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("내 근처"),
+        actions: [
+          Row(
             children: [
-              Text("이웃들의 추천가게"),
-              ListView(),
+              Icon(Icons.cabin_outlined),
+              Icon(Icons.cabin_outlined),
             ],
           ),
-        ),
-      ],
+        ],
+      ), // AppBar
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ), // 앱바 밑에 줄
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(), // 검색창
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView(
+              children: [
+                Wrap(
+                  children: [
+                    Icon(Icons.local_cafe),
+                    Icon(Icons.local_pizza),
+                    // 추가 아이콘들
+                  ],
+                ),
+              ],
+            ),
+          ), // 태그와 아이콘 나열
+          SizedBox(height: 20), // 회색 줄
+          Expanded(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("이웃들의 추천가게"),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        ListTile(
+                          title: Text('가게 1'),
+                        ),
+                        ListTile(
+                          title: Text('가게 2'),
+                        ),
+                        // 추가 가게들
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
